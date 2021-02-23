33c6a55dff735f02f8db6ea3e222044d02068938
Robert Friberg <robert@devrex.se>
2021-02-23 15:14:08 +0100

    sillyioc unit test starter

--- UnitTests/MyNullableTests.cs
+++ UnitTests/MyNullableTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
+
     public class MyNullableTests
     {
         [Test]
---
+++ UnitTests/SillyIocTests.cs
@@ -0,0 +1,48 @@
+﻿using ClassLibrary1;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+    public class SillyIocTests
+    {
+        [Test]
+        public void CanonicalResolution()
+        {
+            IRegistry registry = null;
+            IResolver resolver = null;
+
+            registry.Register<Sha1>();
+
+            var resolved = resolver.Resolve<Sha1>();
+            Assert.IsInstanceOf<Sha1>(resolved);
+        }
+
+        [Test]
+        public void CanResolveInterface()
+        {
+            IRegistry registry = null;
+            IResolver resolver = null;
+
+            registry.Register<Sha1>();
+
+            var resolved = resolver.Resolve<IChecksum>();
+            Assert.IsInstanceOf<Sha1>(resolved);
+        }
+
+        [Test]
+        public void CantResolveInterfaceWhenMultipleCandidatesExist()
+        {
+            IRegistry registry = null;
+            IResolver resolver = null;
+
+            registry.Register<Sha1>();
+            registry.Register<Md5>();
+
+            Assert.Throws<Exception>(() =>
+           {
+               resolver.Resolve<IChecksum>();
+           });
+        }
+    }
+}
